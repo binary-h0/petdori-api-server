0f83aec8d613743b7b3ef0204b7f0561e403b996
Cho-SangHyun <98shcho@naver.com>
2023-08-23 20:19:37 +0900

    refactor(#1): AuthService 리팩터링
    - 회원가입/로그인/토큰 재발급시 토큰들 만들어 dto에 담는 기능 메서드화

--- src/main/java/wooyoungsoo/authserver/domain/auth/service/AuthService.java
+++ src/main/java/wooyoungsoo/authserver/domain/auth/service/AuthService.java
@@ -3,7 +3,6 @@ package wooyoungsoo.authserver.domain.auth.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.parser.ParseException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -43,14 +42,7 @@ public class AuthService {
                 new UsernamePasswordAuthenticationToken(
                         userDetails, "", userDetails.getAuthorities());
 
-        String accessToken = jwtProvider.createAccessToken(authentication);
-        RefreshToken refreshToken = refreshTokenService.createRefreshToken(member);
-
-        return JwtResponseDto
-                .builder()
-                .accessToken(accessToken)
-                .refreshToken(refreshToken.getTokenValue())
-                .build();
+        return generateJwtResponse(authentication, member);
     }
 
     public JwtResponseDto login(Oauth2Provider oauth2Provider, String oauth2Token) {
@@ -75,24 +67,14 @@ public class AuthService {
             Member member = memberRepository.findByEmail(email).orElseThrow(() ->
                     new MemberNotExistException(email));
             WYSMemberDetails userDetails = new WYSMemberDetails(member);
-            log.info("member is present");
+
 
             if (oauth2Provider.equals(userDetails.getOauth2Provider())) {
-                log.info("{}에서 온 유저", oauth2Provider.name());
                 Authentication authentication =
                         new UsernamePasswordAuthenticationToken(
                                 userDetails, "", userDetails.getAuthorities());
 
-                log.info("이제 토큰 만들어유");
-
-                String accessToken = jwtProvider.createAccessToken(authentication);
-                RefreshToken refreshToken = refreshTokenService.createRefreshToken(member);
-                log.info("멤버서비스에서의 값 {}", refreshToken.getTokenValue());
-                return JwtResponseDto
-                        .builder()
-                        .accessToken(accessToken)
-                        .refreshToken(refreshToken.getTokenValue())
-                        .build();
+                return generateJwtResponse(authentication, member);
             }
 
             log.info("다른 곳에서 가입했어요!");
@@ -108,15 +90,12 @@ public class AuthService {
             throws ParseException {
         String email = null;
 
-        log.info("온 토큰 : {}", oauth2Token);
         if (Oauth2Provider.GOOGLE.equals(oauth2Provider)) {
             email = googleEmailExtractor.extractEmailFromGoogleAccessToken(oauth2Token);
         }
-
         if (Oauth2Provider.KAKAO.equals(oauth2Provider)) {
             email = kakaoEmailExtractor.extractEmailFromKakaoAccessToken(oauth2Token);
         }
-
         if (Oauth2Provider.APPLE.equals(oauth2Provider)) {
             email = appleEmailExtractor.extractEmailFromAppleIdToken(oauth2Token);
         }
@@ -128,28 +107,29 @@ public class AuthService {
         RefreshToken oldRefreshToken = refreshTokenService
                 .findOldRefreshTokenByTokenValue(refreshTokenValue);
 
-        log.info("프론트쪽 리토: {}", refreshTokenValue);
-        log.info("디비쪽 리토: {}", oldRefreshToken.getTokenValue());
-
         if (oldRefreshToken != null && refreshTokenValue.equals(oldRefreshToken.getTokenValue())) {
             jwtProvider.validateRefreshToken(refreshTokenValue);
 
             Member member = oldRefreshToken.getMember();
             WYSMemberDetails userDetails = new WYSMemberDetails(member);
-
             Authentication authentication =
                     new UsernamePasswordAuthenticationToken(
                             userDetails, "", userDetails.getAuthorities());
 
-            String newAccessToken = jwtProvider.createAccessToken(authentication);
-            RefreshToken newRefreshToken = refreshTokenService.createRefreshToken(member);
-            return JwtResponseDto
-                    .builder()
-                    .accessToken(newAccessToken)
-                    .refreshToken(newRefreshToken.getTokenValue())
-                    .build();
+            return generateJwtResponse(authentication, member);
         }
 
         throw new RuntimeException("refresh token이 없거나.. 뭐 그렇습니다");
+    }
+
+    private JwtResponseDto generateJwtResponse(Authentication authentication, Member member) {
+        String accessToken = jwtProvider.createAccessToken(authentication);
+        RefreshToken refreshToken = refreshTokenService.createRefreshToken(member);
+
+        return JwtResponseDto
+                .builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken.getTokenValue())
+                .build();
     }
 }
